c0adec0343997c37a8aca2ac9707943b2932e4f5
Yasuo Honda <yasuo.honda@gmail.com>
2013-07-18 03:48:28 +0900

    Create ruby user for ruby-oci8 unit tests

--- puppet/modules/oracle/files/create_rails_users.sql
+++ puppet/modules/oracle/files/create_rails_users.sql
@@ -16,4 +16,9 @@ GRANT unlimited tablespace, create session, create table, create sequence,
 create procedure, create trigger, create view, create materialized view,
 create database link, create synonym, create type, ctxapp TO arunit2;
 
+CREATE USER ruby IDENTIFIED BY oci8;
+GRANT connect, resource, create view TO ruby;
+GRANT EXECUTE ON dbms_lock TO ruby;
+GRANT CREATE VIEW TO ruby;
+
 quit
