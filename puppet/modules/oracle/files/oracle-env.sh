6bbb9e659aba4890a8c4dcd4df43f38ef1ccc6e9
Yasuo Honda <yasuo.honda@gmail.com>
2013-07-17 22:42:49 +0900

    Add some environment variables to execute AR and Oracle enhanced unit test

--- puppet/modules/oracle/files/oracle-env.sh
+++ puppet/modules/oracle/files/oracle-env.sh
@@ -4,3 +4,12 @@ export NLS_LANG=`$ORACLE_HOME/bin/nls_lang.sh`
 export ORACLE_BASE=/u01/app/oracle
 export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
 export PATH=$ORACLE_HOME/bin:$PATH
+
+export ORACLE_ENHANCED=true
+export DATABASE_SYS_PASSWORD=oracle
+export DATABASE_VERSION=11.2.0.2
+export ARUNIT_DB_NAME=${ORACLE_SID}
+export DATABASE_HOST=localhost
+export DATABASE_PORT=1521
+export DATABASE_NAME=${ORACLE_SID}
+
