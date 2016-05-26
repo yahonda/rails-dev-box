e2c0adc1b9fdcbc38e02dbf55c43b46ce3022e5c
Yasuo Honda <yasuo.honda@gmail.com>
2016-05-26 13:58:07 +0000

    Install JRuby for ActiveRecord JDBC Adapter

--- Vagrantfile
+++ Vagrantfile
@@ -7,6 +7,7 @@ Vagrant.configure('2') do |config|
   config.vm.network :forwarded_port, guest: 3000, host: 3000
 
   config.vm.provision :shell, path: 'bootstrap.sh', keep_color: true
+  config.vm.provision :shell, path: 'bootstrap_arjdbc.sh', keep_color: true
 
   config.vm.provider 'virtualbox' do |v|
     v.memory = 1024
---
+++ bootstrap_arjdbc.sh
@@ -0,0 +1,38 @@
+# The output of all these installation steps is noisy. With this utility
+# the progress report is nice and concise.
+function install {
+    echo installing $1
+    shift
+    apt-get -y install "$@" >/dev/null 2>&1
+}
+
+echo installing OS packages
+install OpenJDK openjdk-8-jdk
+install Ant ant
+
+echo setting up rbenv
+sudo -u vagrant -i git clone https://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv
+sudo -u vagrant -i echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.bashrc
+sudo -u vagrant -i echo 'eval "$(rbenv init -)"' >> /home/vagrant/.bashrc
+sudo -u vagrant -i git clone https://github.com/sstephenson/ruby-build.git /home/vagrant/.rbenv/plugins/ruby-build
+
+echo installing JRuby
+sudo -H -u vagrant bash -i -c 'source  /home/vagrant/.bashrc'
+sudo -H -u vagrant bash -i -c 'rbenv install jruby-9.1.1.0'
+sudo -H -u vagrant bash -i -c 'rbenv global jruby-9.1.1.0'
+sudo -H -u vagrant bash -i -c 'gem install bundle'
+
+echo setting up PostgreSQL
+sudo -u vagrant -i echo 'export PGHOST=localhost' >> /home/vagrant/.bashrc
+rm /etc/postgresql/9.4/main/pg_hba.conf
+echo 'local all all trust' >> /etc/postgresql/9.4/main/pg_hba.conf
+echo 'host all all 127.0.0.1/32 trust' >> /etc/postgresql/9.4/main/pg_hba.conf
+echo 'host all all ::1/128 trust' >> /etc/postgresql/9.4/main/pg_hba.conf
+service postgresql restart
+echo "create user rails superuser;" | sudo -u postgres psql
+
+echo setting up MySQL
+mysql -uroot -proot <<SQL
+set password = '';
+SQL
+echo 'rails-dev-box runs JRuby now!'
