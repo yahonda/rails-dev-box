040dd7697c16e707dd081bc3b8bfa574f967ef1c
Yasuo Honda <yasuo.honda@gmail.com>
2017-01-11 16:25:52 +0000

    Install SQL Server for Linux and create database users for ActiveRecord SQL Server Adapter
    - Increase memory size to 4GB to run SQL Server on Linux
    
    Thanks to these documents:
    https://github.com/rails-sqlserver/activerecord-sqlserver-adapter/blob/master/RUNNING_UNIT_TESTS.md
    https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-setup-ubuntu
    https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-setup-tools#ubuntu
    https://msdn.microsoft.com/en-us/library/hh568454(v=sql.110)

---
+++ bootstrap_sqlserver.sh
@@ -0,0 +1,36 @@
+export DEBIAN_FRONTEND=noninteractive
+
+# Install mssql-server
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/16.04/mssql-server.list | sudo tee /etc/apt/sources.list.d/mssql-server.list
+
+apt-get update
+apt-get install -y mssql-server
+
+export SA_PASSWORD=Sqlpasswd1
+sudo -E /opt/mssql/bin/sqlservr-setup --accept-eula --set-sa-password --start-service --enable-service
+
+# Install mssql-tools
+
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+export ACCEPT_EULA=Y
+sudo apt-get update
+sudo -E apt-get -y install mssql-tools unixodbc-dev-utf16 msodbcsql unixodbc-utf16 libodbc1-utf16
+ln -sfn /opt/mssql-tools/bin/sqlcmd-13.0.1.0 /usr/bin/sqlcmd
+
+# create databases
+sqlcmd -S localhost -U SA -P $SA_PASSWORD <<EOT
+CREATE DATABASE [activerecord_unittest];
+CREATE DATABASE [activerecord_unittest2];
+GO
+CREATE LOGIN [rails] WITH PASSWORD = '', CHECK_POLICY = OFF, DEFAULT_DATABASE = [activerecord_unittest];
+GO
+USE [activerecord_unittest];
+CREATE USER [rails] FOR LOGIN [rails];
+GO
+EXEC sp_addrolemember N'db_owner', N'rails';
+EXEC master..sp_addsrvrolemember @loginame = N'rails', @rolename = N'sysadmin'
+GO
+exit
+EOT
