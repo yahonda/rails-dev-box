6b18137176c0e75265544ec5ee5706550af78bea
Yasuo Honda <yasuo.honda@gmail.com>
2017-07-27 15:47:00 -0400

    Two cosmetic changes * DATABASE_PASSWORD can be given by `-e ORACLE_PWD` option * vagrant up will wait for until Oracle database is ready

--- bootstrap_docker_on_oracle.sh
+++ bootstrap_docker_on_oracle.sh
@@ -17,7 +17,7 @@ sudo -u vagrant -i cp /vagrant/linuxx64_12201_database.zip \
   /home/vagrant/docker-images/OracleDatabase/dockerfiles/12.2.0.1/.
 cd /home/vagrant/docker-images/OracleDatabase/dockerfiles
 sudo ./buildDockerImage.sh -v 12.2.0.1 -e
-sudo docker run -d -p 1521:1521 --name yahonda oracle/database:12.2.0.1-ee
+sudo docker run -d -p 1521:1521 -e ORACLE_PWD=admin --name oracle-container oracle/database:12.2.0.1-ee
 
 echo setting up Oracle client
 sudo -u vagrant -i cp /vagrant/oracle-instantclient12.2-basic-12.2.0.1.0-1.x86_64.rpm \
@@ -41,6 +41,6 @@ sudo -u vagrant -i echo 'export DATABASE_NAME=ORCLPDB1' \
   >> /home/vagrant/.bashrc
 
 echo showing Oracle database status. Wait until "DATABASE IS READY TO USE!" message appears.
-docker logs `docker ps -q`
+grep -m 1 "DATABASE IS READY TO USE!" <(sudo docker logs -f oracle-container)
 
 echo 'rails-dev-box runs Oracle on Docker now!'
