189aa8ca44d6cb817f94fb19950fbd5929121f3a
Yasuo Honda <yasuo.honda@gmail.com>
2019-03-02 03:19:20 +0000

    Use Oracle Database Express Edition (XE) Release 18.4.0.0.0 (18c)
    * Use 'bento/ubuntu-18.10' Vagrant Box
    * Bump Oracle Instant Client minor versions
    * Change Oracle user password to `admin123` to address ORA-01017

--- bootstrap_docker_on_oracle.sh
+++ bootstrap_docker_on_oracle.sh
@@ -11,25 +11,24 @@ install Docker docker.io
 install Alien alien
 
 echo setting up Oracle database server on Docker
-sudo -u vagrant -i git clone -b runs_oracle_enhanced_18c https://github.com/yahonda/docker-images.git \
-  /home/vagrant/docker-images
-sudo -u vagrant -i cp /vagrant/LINUX.X64_180000_db_home.zip \
-  /home/vagrant/docker-images/OracleDatabase/SingleInstance/dockerfiles/18.3.0/.
+sudo -u vagrant -i git clone https://github.com/oracle/docker-images.git /home/vagrant/docker-images
+sudo -u vagrant -i cp /vagrant/oracle-database-xe-18c-1.0-1.x86_64.rpm \
+  /home/vagrant/docker-images/OracleDatabase/SingleInstance/dockerfiles/18.4.0/.
 cd /home/vagrant/docker-images/OracleDatabase/SingleInstance/dockerfiles/
-sudo ./buildDockerImage.sh -e -v 18.3.0
-sudo docker run -d -p 1521:1521 -e ORACLE_PWD=admin --name oracle-container oracle/database:18.3.0-ee
+sudo ./buildDockerImage.sh -x -v 18.4.0
+sudo docker run -d -p 1521:1521 -e ORACLE_PWD=admin123 --name oracle-container oracle/database:18.4.0-xe
 
 echo setting up Oracle client
-sudo -u vagrant -i cp /vagrant/oracle-instantclient18.3-basic-18.3.0.0.0-1.x86_64.rpm \
+sudo -u vagrant -i cp /vagrant/oracle-instantclient18.3-basic-18.3.0.0.0-3.x86_64.rpm \
   /home/vagrant/.
-sudo -u vagrant -i cp /vagrant/oracle-instantclient18.3-devel-18.3.0.0.0-1.x86_64.rpm \
+sudo -u vagrant -i cp /vagrant/oracle-instantclient18.3-devel-18.3.0.0.0-3.x86_64.rpm \
   /home/vagrant/.
-sudo -u vagrant -i cp /vagrant/oracle-instantclient18.3-sqlplus-18.3.0.0.0-1.x86_64.rpm \
+sudo -u vagrant -i cp /vagrant/oracle-instantclient18.3-sqlplus-18.3.0.0.0-3.x86_64.rpm \
   /home/vagrant/.
 
-alien -i /home/vagrant/oracle-instantclient18.3-basic-18.3.0.0.0-1.x86_64.rpm
-alien -i /home/vagrant/oracle-instantclient18.3-devel-18.3.0.0.0-1.x86_64.rpm
-alien -i /home/vagrant/oracle-instantclient18.3-sqlplus-18.3.0.0.0-1.x86_64.rpm
+alien -i /home/vagrant/oracle-instantclient18.3-basic-18.3.0.0.0-3.x86_64.rpm
+alien -i /home/vagrant/oracle-instantclient18.3-devel-18.3.0.0.0-3.x86_64.rpm
+alien -i /home/vagrant/oracle-instantclient18.3-sqlplus-18.3.0.0.0-3.x86_64.rpm
 
 sudo -u vagrant -i echo 'export PATH=/usr/lib/oracle/18.3/client64/bin:$PATH' \
   >> /home/vagrant/.bashrc
@@ -37,7 +36,7 @@ sudo -u vagrant -i echo 'export LD_LIBRARY_PATH=/usr/lib/oracle/18.3/client64/li
   >> /home/vagrant/.bashrc
 sudo -u vagrant -i echo 'export NLS_LANG=American_America.AL32UTF8' \
   >> /home/vagrant/.bashrc
-sudo -u vagrant -i echo 'export DATABASE_NAME=ORCLPDB1' \
+sudo -u vagrant -i echo 'export DATABASE_NAME=XEPDB1' \
   >> /home/vagrant/.bashrc
 
 echo showing Oracle database status. Wait until "DATABASE IS READY TO USE!" message appears.
